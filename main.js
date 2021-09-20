533dc7e9e708e974aa280394303a519db5497e8c
anuakshat <53713926+anuakshat@users.noreply.github.com>
2021-09-20 19:23:59 +0530

    feat : profit/loss of share

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const numOfShares=document.querySelector("#number-of-shares");
 const purchasePrice=document.querySelector("#purchase-price");
 const currentPrice=document.querySelector("#current-price");
 const calculateButton=document.querySelector("#calculate");
-const output=document.querySelector("absolute-output");
-const percentOutput=document.querySelector("#percentage-output");
+// const output=document.querySelector("absolute-output");
+// const percentOutput=document.querySelector("#percentage-output");
 const warn= document.querySelector("#warning");
 
 function profitorLoss(purchase,current,number){
@@ -20,18 +20,28 @@ function percententaged(purchase,current,number){
 
 
 function clickHandler(){
-console.log("akshat");
-    //     if(numOfShares.value,purchasePrice.value,currentPrice.value!==""){
-//         const number=numOfShares.value;
-// const purchased = purchasePrice.value;
-// const currents=currentPrice.value;
-// output.innerText="your profit is "+profitorLoss(number,purchased,currents);
-// percentOutput.innerText="your percent return is"+percententaged(number,purchased,currents);
 
-//     }
-//     else{
-// warn.innerText="please fill all the given fields";
-//     }
+    const number=numOfShares.value;
+const purchased = purchasePrice.value;
+const currents=currentPrice.value;
+    
+    if(numOfShares.value,purchasePrice.value,currentPrice.value!==''){
+        const absoluteReturn=profitorLoss(purchased,currents,number);
+const percentageReturn=percententaged(purchased,currents,number);
+// output.innerText="your profit is "+ profitorLoss(number,purchased,currents);
+// percentOutput.innerText="your percent return is"+percententaged(number,purchased,currents);
+if(absoluteReturn<0){
+   const negativeReturn=-(absoluteReturn);
+   const negativePercent=-(percentageReturn);
+    warn.innerText=`your loss is ${negativeReturn} you have losted ${negativePercent}% of your wealth`;
+    }
+    else{warn.innerText=`you profit is ${absoluteReturn} you have earned${percentageReturn}% from your wealth`;
+
+    }
+}
+    else{
+warn.innerText="please fill all the given fields";
+    }
 }
 
 calculateButton.addEventListener("click", clickHandler);
