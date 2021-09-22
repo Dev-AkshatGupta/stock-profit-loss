1a727a8b181d1cf3264b74d9516e19744b1054b4
anuakshat <53713926+anuakshat@users.noreply.github.com>
2021-09-22 16:24:11 +0530

    feat: warning for negative values

--- main.js
+++ main.js
@@ -1,50 +1,57 @@
-const numOfShares=document.querySelector("#number-of-shares");
-const purchasePrice=document.querySelector("#purchase-price");
-const currentPrice=document.querySelector("#current-price");
-const calculateButton=document.querySelector("#calculate");
+const numOfShares = document.querySelector("#number-of-shares");
+const purchasePrice = document.querySelector("#purchase-price");
+const currentPrice = document.querySelector("#current-price");
+const calculateButton = document.querySelector("#calculate");
 // const output=document.querySelector("absolute-output");
 // const percentOutput=document.querySelector("#percentage-output");
-const warn= document.querySelector("#warning");
+const warn = document.querySelector("#warning");
 
-function profitorLoss(purchase,current,number){
-    const diffrence=current-purchase;
-    const total=diffrence*number;
+function profitorLoss(purchase, current, number) {
+    const diffrence = current - purchase;
+    const total = diffrence * number;
 
     return total;
 }
-function percententaged(purchase,current,number){
-    const pL=profitorLoss(purchase,current,number);
-    const percentage=pL*100/purchase/number;
+
+function percententaged(purchase, current, number) {
+    const pL = profitorLoss(purchase, current, number);
+    const percentage = pL * 100 / purchase / number;
     return percentage;
 }
 
 
-function clickHandler(){
+function clickHandler() {
 
-    const number=numOfShares.value;
-const purchased = purchasePrice.value;
-const currents=currentPrice.value;
-    
-    if(numOfShares.value,purchasePrice.value,currentPrice.value!==''){
-        const absoluteReturn=profitorLoss(purchased,currents,number);
-const percentageReturn=percententaged(purchased,currents,number);
-const percent=percentageReturn.toFixed(2);
+    const number = numOfShares.value;
+    const purchased = purchasePrice.value;
+    const currents = currentPrice.value;
+
+    if (numOfShares.value, purchasePrice.value, currentPrice.value !== '')
+     {
+        if (numOfShares.value || purchasePrice.value || currentPrice.value <= 0)
+        {
+            warn.innerText="Please enter valid values"
+
+        } else 
+        {
+            const absoluteReturn = profitorLoss(purchased, currents, number);
+            const percentageReturn = percententaged(purchased, currents, number);
+            const percent = percentageReturn.toFixed(2);
 
 
-if(absoluteReturn<0){
-   const negativeReturn=-(absoluteReturn);
-   const negativePercent=-(percent);
-   warn.style.color='red';
-    warn.innerText=`your loss is ${negativeReturn} you have losted ${negativePercent}% of your wealth`;
-    }
-    else{
-      warn.style.color='green';
-      warn.innerText=`you profit is ${absoluteReturn} you have earned ${percent}% from your wealth`;
+            if (absoluteReturn < 0) {
+                const negativeReturn = -(absoluteReturn);
+                const negativePercent = -(percent);
+                warn.style.color = 'red';
+                warn.innerText = `your loss is ${negativeReturn} you have losted ${negativePercent}% of your wealth`;
+            }
+         else {
+            warn.style.color = 'green';
+            warn.innerText = `you profit is ${absoluteReturn} you have earned ${percent}% from your wealth`;
 
-    }
-}
-    else{
-warn.innerText="please fill all the given fields";
+        }}
+    } else {
+        warn.innerText = "please fill all the given fields";
     }
 }
 
